117d80084bbb3d85c9a576caec069b3eba7a3447
Kio-dev-it <pas.petito@gmail.com>
2021-10-06 15:58:53 +0200

    Add flag to language using a package

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -2,15 +2,19 @@
     <div class="movie-card">
             <div><strong>Titolo:</strong>{{info.title}}</div>
             <div><strong>Titolo orginale:</strong>{{info.original_title}}</div>
-            <div><strong>lingua:</strong>{{info.original_language}}</div>
+            <div><strong>lingua:</strong><lang-flag :iso="info.original_language" :squared="false"/></div>
             <div><strong>Voto:</strong>{{info.vote_average}}</div>
     </div>
 </template>
 
 <script>
+import LangFlag from 'vue-lang-code-flags';
 export default {
     name: 'MovieCard',
     props:['info'],
+    components:{
+        LangFlag
+    }
 }
 </script>
 
