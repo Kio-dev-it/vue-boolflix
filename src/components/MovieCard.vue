a50df59d400dfab9ccc15ab080f25960d0fd43b3
Kio-dev-it <pas.petito@gmail.com>
2021-10-06 23:16:24 +0200

    Add star rating methods instead of vote

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -14,17 +14,35 @@
                 <div><strong>Titolo orginale:</strong>{{info.original_name}}</div>
             </div>
             <div><strong>lingua:</strong><lang-flag :iso="info.original_language" :squared="false"/></div>
-            <div><strong>Voto:</strong>{{info.vote_average}}</div>
+            <div><strong>Voto:</strong><font-awesome-icon :icon="starSolid" v-for="elm in starMaker()" :key="elm"/><font-awesome-icon :icon="starEmpty" v-for="elm in 5 - starMaker()" :key="elm"/></div>
     </div>
 </template>
 
 <script>
 import LangFlag from 'vue-lang-code-flags';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+
+
 export default {
     name: 'MovieCard',
     props:['info'],
     components:{
-        LangFlag
+        LangFlag,
+        FontAwesomeIcon,
+    }, 
+    data() {
+        return {
+            vote: 0,
+            starSolid: fasStar,
+            starEmpty: farStar
+        }
+    },
+    methods:{
+        starMaker() {
+            return Math.ceil(this.info.vote_average / 2);
+        }
     }
 }
 </script>
