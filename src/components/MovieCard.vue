1bca7be01b1a75923dc6aad0228d4bf6c1d0767c
Kio-dev-it <pas.petito@gmail.com>
2021-10-06 16:43:58 +0200

    Add search in TV Show API

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,7 +1,13 @@
 <template>
-    <div class="movie-card">
-            <div><strong>Titolo:</strong>{{info.title}}</div>
-            <div><strong>Titolo orginale:</strong>{{info.original_title}}</div>
+    <div class="card">
+            <div class="card__movie" v-if="info.original_title">
+                <div><strong>Titolo:</strong>{{info.title}}</div>
+                <div><strong>Titolo orginale:</strong>{{info.original_title}}</div>
+            </div>
+            <div class="card__series" v-if="info.original_name">
+                <div><strong>Titolo:</strong>{{info.name}}</div>
+                <div><strong>Titolo orginale:</strong>{{info.original_name}}</div>
+            </div>
             <div><strong>lingua:</strong><lang-flag :iso="info.original_language" :squared="false"/></div>
             <div><strong>Voto:</strong>{{info.vote_average}}</div>
     </div>
--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,6 +1,13 @@
 <template>
     <main>
+        <h2>Movies</h2>
         <ul v-for="elm in movieInfo" :key="elm.id">
+            <li>
+                <MovieCard :info="elm"/>
+            </li>
+        </ul>
+        <h2>TV Series</h2>
+        <ul v-for="elm in seriesInfo" :key="elm.id">
             <li>
                 <MovieCard :info="elm"/>
             </li>
@@ -20,11 +27,14 @@ export default {
     },
     data() {
         return {
-            movieInfo: []
+            movieInfo: [], 
+            seriesInfo: []
         }
     },
     watch:{
         infoSearch: function() {
+            
+            //call for movie search
             axios
                 .get("https://api.themoviedb.org/3/search/movie", {
                     params: {
@@ -37,7 +47,22 @@ export default {
                 .then(
                     (resp)=>{
                         this.movieInfo = resp.data.results;
-                        console.log(this.movieInfo);
+                    }
+                );
+            
+            //call for TV series search
+            axios
+                .get("https://api.themoviedb.org/3/search/tv", {
+                    params: {
+                        api_key: '5f982b7a134b61a8191ea027b951c118',
+                        query: this.infoSearch,
+                        language: 'it-IT'
+                    }
+                }
+                )
+                .then(
+                    (resp)=>{
+                        this.seriesInfo = resp.data.results;
                     }
                 )
         }
